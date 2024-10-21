810df970caf246bbf598c223c8a9484f7fe4b04e
tjrwls0826 <tjrwls0826@naver.com>
2024-10-22 03:01:10 +0900

    배열 실습

---
+++ src/자료형 연산자 키보드입력/Array6.java
@@ -0,0 +1,7 @@
+public class Array6 {
+	public static void main(String[] args) {
+		double arr[] = {1.1, 2.3, 4.7, 7.5 };
+		
+		System.out.printf("배열의 크기 : %d\n%f\n%f\n%f\n%f", arr.length, arr[0], arr[1], arr[2], arr[3]);
+	}
+}
---
+++ src/자료형 연산자 키보드입력/Array7.java
@@ -0,0 +1,12 @@
+public class Array7{
+	public static void main(String[] args){
+		double arr[] = new double[4];
+		
+		arr[0] = 1.1;
+		arr[1] = 2.2;
+		arr[2] = 3.3;
+		arr[3] = 4.4;
+		
+		System.out.printf("배열의 크기 : %d\n%.1f\n%.1f\n%.1f\n%.1f", arr.length, arr[0], arr[1], arr[2], arr[3]);
+	}
+}

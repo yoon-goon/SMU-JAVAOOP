b4b3a4b2a2dab12c3a3aa40b049940b085ee8c61
tjrwls0826 <tjrwls0826@naver.com>
2024-10-21 23:23:21 +0900

    Scanner 기초 실습

---
+++ src/midtermPrac/자료형 연산자 키보드입력/Scanner2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+class ReadLine {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.nextLine();
+		
+		System.out.printf("\n입력된 문자열: %s",a);
+		
+	}
+}

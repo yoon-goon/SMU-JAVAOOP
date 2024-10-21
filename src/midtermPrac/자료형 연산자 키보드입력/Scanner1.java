1821ebae8f9a9d561da3c75846875e96e8135be4
tjrwls0826 <tjrwls0826@naver.com>
2024-10-21 23:19:16 +0900

    Update Scanner1.java

--- src/midtermPrac/자료형 연산자 키보드입력/Scanner1.java
+++ src/midtermPrac/자료형 연산자 키보드입력/Scanner1.java
@@ -4,9 +4,9 @@ class Scanner1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); // 키보드 입력
 		String a = sc.next();
-		System.out.printf("문자열 출력: %s\n",a);
+		System.out.println("문자열 출력: %s\n",a);
 		int b = sc.nextInt();
-		System.out.printf("정수 출력: %d", b);
+		System.out.println("정수 출력: %d", b);
 		}
 }
 

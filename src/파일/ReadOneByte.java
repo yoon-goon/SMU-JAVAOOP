215f28f99bb94c31e4f32188e781d564b3da6d6c
tjrwls0826 <tjrwls0826@naver.com>
2024-11-19 14:41:19 +0900

    파일 readOneByte

---
+++ src/파일/ReadOneByte.java
@@ -0,0 +1,18 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadOneByte {
+    public static void main(String[] args) {
+        try {
+            FileInputStream f = new FileInputStream("hello.txt");
+            int data = f.read();
+            while (data != -1) {
+                System.out.print((char) data);
+                data = f.read(); // 다음 바이트 읽기
+            }
+            f.close(); // 스트림 닫기
+        } catch (IOException e) {
+            e.printStackTrace(); // 예외 출력
+        }
+    }
+}
